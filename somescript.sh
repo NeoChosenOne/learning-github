f7717a28998d4d07195e07babebf9aa0da744196
Neo <n.brandeiz@web.de>
2025-04-25 11:08:06 +0200

    rewritten

--- somescript.sh
+++ somescript.sh
@@ -1,6 +1,6 @@
 # Notes
 To save the written code type:
-git checkout "newbranch" or
+git checkout -b <name> or
 git add . = Adds it to the already existing branch
 git commit
 git push --set-upstream origin "Branch you're working in"
