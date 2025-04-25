41753dbadae644ef701d52e93f9c20e8b28f441a
Neo <n.brandeiz@web.de>
2025-04-25 11:01:56 +0200

    guide

--- somescript.sh
+++ somescript.sh
@@ -1,4 +1,9 @@
 # Notes
+To save the written code type:
+git checkout "newbranch" or
+git add . = Adds it to the already existing branch
+git commit
+git push --set-upstream origin "Branch you're working in"
 
 '''
 testing
